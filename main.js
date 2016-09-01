d418caa700eadb353e3f18ae0fadecd913bcbf35
substack <substack@9nTgtYmvW4HID6ayt6Icwc8WZxdifx5SlSKKIX/X/1g=.ed25519>
2016-09-01 15:53:02 -0500

    fucked up floor

--- main.js
+++ main.js
@@ -9,7 +9,6 @@ var resl = require('resl')
 var parseobj = require('parse-wavefront-obj')
 var scom = require('simplicial-complex')
 var column = require('column-mesh')
-var sphere = require('sphere-mesh')
 var normals = require('angle-normals')
 
 var iframe = document.createElement('iframe')
@@ -29,22 +28,83 @@ resl({
     }
   },
   onDone: function (data) {
-    var drawShrek =  shrek(data)
-    var drawCol = col()
+    var draw = {
+      shrek: shrek(data),
+      col: col(),
+      floor: floor()
+    }
     var cols = []
     for (var i = 0; i < 20; i++) {
       var theta = (i/20*2-1) * Math.PI
-      cols.push({ location: [-100*Math.sin(theta),0,100*Math.cos(theta)] })
+      cols.push({ location: [-100*Math.sin(theta),10.5,100*Math.cos(theta)] })
     }
     regl.frame(function () {
       regl.clear({ color: [0,0,0,1] })
       camera(function () {
-        drawShrek()
-        drawCol(cols)
+        draw.shrek()
+        draw.col(cols)
+        draw.floor()
       })
     })
   }
 })
+
+function floor () {
+  var positions = []
+  for (var x = -10; x <= 10; x++) {
+    for (var y = -10; y <= 10; y++) {
+      positions.push([x,0,y])
+    }
+  }
+  var cells = []
+  for (var x = -10; x < 10; x++) {
+    for (var y = -10; y < 10; y++) {
+      var p0 = (x+0+10)*20 + (y+0+10)
+      var p1 = (x+1+10)*20 + (y+0+10)
+      var p2 = (x+1+10)*20 + (y+1+10)
+      var p3 = (x+0+10)*20 + (y+1+10)
+      cells.push([ p0, p1 ])
+      cells.push([ p1, p2 ])
+      cells.push([ p2, p3 ])
+      cells.push([ p3, p0 ])
+    }
+  }
+
+  var model = []
+  return regl({
+    frag: `
+      precision mediump float;
+      varying vec2 pos;
+      void main () {
+        float r = abs(sin(pos.x/5.0 + pos.y/5.0));
+        float g = abs(sin(pos.y/3.0 + sin(pos.y/2.0)));
+        float b = abs(sin(pos.x/10.0 + pos.y/10.0));
+        gl_FragColor = vec4(r,g,b,1);
+      }
+    `,
+    vert: `
+      precision mediump float;
+      uniform mat4 projection, view, model;
+      attribute vec3 position;
+      varying vec2 pos;
+      void main () {
+        pos = position.xz;
+        gl_Position = projection * view * model * vec4(position,1.0);
+      }
+    `,
+    attributes: {
+      position: positions
+    },
+    uniforms: {
+      model: function (context, props) {
+        mat4.identity(model)
+        mat4.scale(model, model, [50,50,50])
+        return model
+      }
+    },
+    elements: cells
+  })
+}
 
 function col () {
   var mesh = column()
@@ -126,6 +186,7 @@ function shrek (data) {
     uniforms: {
       model: function (context, props) {
         mat4.identity(model)
+        mat4.translate(model, model, [0,10,0])
         mat4.rotateY(model, model, Math.PI/2 + Math.sin(context.time))
         return model
       },
